cd34f9b7c510c31b78203039937887460cda3f84
sunpreetkp <sunpreetkp2003@gmail.com>
2022-12-12 18:16:19 -0500

    final commit

---
+++ app.js
@@ -0,0 +1,34 @@
+/********************************************************************************* 
+*  WEB322 – Assignment 6 
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.   
+*  No part of this assignment has been copied manually or electronically from any other source 
+*  (including web sites) or distributed to other students. 
+*  
+*  Name: Sunpreet Kaur Student ID: 163091218 Date: 12 December 2022
+* 
+*  Online (Cyclic) URL: _______________________________________________________ 
+* 
+********************************************************************************/  
+
+const express = require('express')
+
+const app = express()
+
+const port = process.env.port || 3500
+
+app.set('view-engine','ejs')
+
+app.get('/',(req,res)=>
+{
+    res.send("Welcome from the first cyclic demo app!!!")
+})
+
+app.get('/display',(req,res)=>{
+
+    res.render("display.ejs",{author:'Sunpreet Kaur'})
+})
+
+app.listen(port,()=>{
+
+    console.log("App is listening at port 3500-----")
+})
